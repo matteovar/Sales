220fb93f420122575cd138abaf8e3a4ae9c57b0d
matteovar <matvarnier@gmail.com>
2025-03-31 14:50:39 -0300

    New additions

--- app/app.py
+++ app/app.py
@@ -1,14 +1,152 @@
-import streamlit as st
 import pandas as pd
+import plotly.express as px
+import streamlit as st
 from src.main import read_data
 from src.utils.cards import create_cards
 
-st.set_page_config(page_title="Billaboard Top Songs", layout="wide")
+st.set_page_config(page_title="Sales", layout="wide")
 
-df, df_products,df_products_sales = read_data()
+(
+    df,
+    df_products,
+    df_products_sales,
+    df_total_discount,
+    df_prod_disc_name,
+    vendas1,
+    porcentagem,
+    most_common_gender,
+    most_gender,
+    df_sale_date,
+    seila,
+    nao_sei,
+    df_age,
+    loyalty_counts,
+    df_fees,
+) = read_data()
 
 cols_card = st.columns(4)
 
 with cols_card[0]:
-    create_cards("Produto mais vendido", df_products,f'Quantidade de vendas: {df_products_sales}')
-st.dataframe(df)    
+    create_cards(
+        "Produto Mais Vendido",
+        df_products,
+        f"Quantidade de Vendas: {df_products_sales}",
+    )
+with cols_card[1]:
+    create_cards(
+        "Valor Total de Desconto",
+        f"R$ {df_total_discount:.2f}",
+        f"Produto com Maior Desconto: {df_prod_disc_name} ",
+    )
+with cols_card[2]:
+    create_cards(
+        "Consumidores com Transações Completas",
+        vendas1,
+        f"Porcentagem sobre o Tota: {porcentagem}%",
+    )
+with cols_card[3]:
+    create_cards(
+        "Gênero Mais Predominante entre Consumidores",
+        most_common_gender,
+        f"Valor Gasto pelo Gênero {most_common_gender}: R$ {most_gender:.2f}",
+    )
+
+
+def sale_date():
+    st.markdown(
+        """
+        ### Valor Total Vendido de Produtos no Dia
+        """
+    )
+    st.line_chart(df_sale_date)
+
+
+sale_date()
+
+cols = st.columns(2)
+
+with cols[0]:
+    st.markdown(
+        """
+        ### Valor Total Vendido de Produtos por Região
+                """
+    )
+    st.bar_chart(seila)
+with cols[1]:
+    st.markdown(
+        """
+        ###  Valor Vendido de Produtos por Categoria
+                """
+    )
+    seleciona_cat = st.selectbox(
+        "Selecione uma categoria",
+        options=nao_sei["ProductCategory"].unique(),
+        placeholder="Selecione",
+    )
+
+    filter_cat = nao_sei[nao_sei["ProductCategory"] == seleciona_cat]
+    df_filtered = filter_cat.drop(columns=["ProductCategory"])
+
+    st.bar_chart(df_filtered, x="ProductName", y="FinalSalePrice")
+
+cols_customer = st.columns(2)
+
+with cols_customer[0]:
+    st.markdown(
+        """
+        ### Quantidade de Pessoas por Faixa Etária
+                """
+    )
+    fig = px.histogram(
+        df_age,
+        y="CustomerID",
+        x="CustomerAge",
+        log_y=True,
+        color_discrete_sequence=["indianred"],
+        text_auto=True,
+    )
+    fig.update_layout(bargap=0.2)
+    st.plotly_chart(fig)
+
+with cols_customer[1]:
+    st.markdown(
+        """
+        ###  Valor Total dos Produtos no Dia
+                """
+    )
+    fig2 = px.pie(loyalty_counts, names="LoyaltyStatus", values="Count")
+    st.plotly_chart(fig2)
+
+
+def fees_region():
+    st.markdown(
+        """
+        ###    Top 10 Maiores Taxas de Frete por Localização
+
+                """
+    )
+    select_region = st.selectbox(
+        "Selecione uma regiao",
+        options=df["Region"].unique(),
+        index=0,
+    )
+
+    df_shipping_top10 = df_fees[df_fees["Region"] == select_region].nlargest(
+        10, "ShippingFee"
+    )
+
+    fig3 = px.bar(
+        df_shipping_top10,
+        x="CustomerLocation",
+        y="ShippingFee",
+        color="CustomerLocation",
+        labels={
+            "ShippingFee": "Frete Médio (R$)",
+            "CustomerLocation": "Local do Cliente",
+        },
+        text_auto=".2f",
+    )
+    st.plotly_chart(fig3, use_container_width=True)
+
+
+fees_region()
--- app/src/main.py
+++ app/src/main.py
@@ -1,9 +1,77 @@
 import pandas as pd
 
+
 def read_data():
-    df = pd.read_csv('app/data/input/Sales_Data.csv')
-    
-    df_prod = df.groupby(["ProductID"])["SalesQuantity"].sum().reset_index().sort_values("SalesQuantity", ascending = False)
-    df_products = df_prod.iloc[0]["ProductID"]
+    df = pd.read_csv("app/data/input/Sales_Data.csv")
+
+    df_prod = (
+        df.groupby(["ProductName"])["SalesQuantity"]
+        .sum()
+        .reset_index()
+        .sort_values("SalesQuantity", ascending=False)
+    )
+    df_products = df_prod.iloc[0]["ProductName"]
     df_products_sales = df_prod.iloc[0]["SalesQuantity"]
-    return df, df_products,df_products_sales
+
+    df_total_discount = df["DiscountAmount"].sum()
+    df_prod_disc = (
+        df.groupby(["ProductName"])["DiscountAmount"]
+        .sum()
+        .reset_index()
+        .sort_values("DiscountAmount", ascending=False)
+    )
+    df_prod_disc_name = df_prod_disc.iloc[0]["ProductName"]
+
+    vendas = df.loc[df["TransactionStatus"] == "Completed", ["CustomerID"]].count()
+    vendas1 = vendas.loc["CustomerID"]
+
+    consumidores_totais = df["CustomerID"].count()
+
+    porcentagem = (vendas1 / consumidores_totais) * 100
+
+    most_common_gender = df["CustomerGender"].value_counts().idxmax()
+
+    most_gender = df.loc[
+        df["CustomerGender"] == most_common_gender, ["FinalSalePrice"]
+    ].sum()
+    most_gender_fi = most_gender.loc["FinalSalePrice"]
+
+    df_sale_date = df.groupby(["TransactionDate"])["FinalSalePrice"].sum()
+
+    seila = df.groupby(["Region"])["FinalSalePrice"].sum()
+
+    nao_sei = (
+        df.groupby(["ProductCategory", "ProductName"])["FinalSalePrice"]
+        .sum()
+        .reset_index()
+    )
+
+    df_age = df.groupby(["CustomerAge"])["CustomerID"].count().reset_index()
+
+    loyalty_counts = df["LoyaltyProgramMember"].value_counts().reset_index()
+    loyalty_counts.columns = ["LoyaltyStatus", "Count"]
+
+    df_fees = (
+        df.groupby(["Region", "CustomerLocation"])["ShippingFee"]
+        .mean()
+        .reset_index()
+        .sort_values("ShippingFee", ascending=False)
+    )
+
+    return (
+        df,
+        df_products,
+        df_products_sales,
+        df_total_discount,
+        df_prod_disc_name,
+        vendas1,
+        porcentagem,
+        most_common_gender,
+        most_gender_fi,
+        df_sale_date,
+        seila,
+        nao_sei,
+        df_age,
+        loyalty_counts,
+        df_fees,
+    )
